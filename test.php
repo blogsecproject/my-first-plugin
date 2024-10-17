262ee0eda99dc9d9d5727c89c0f5b01eeda14422
blogsecproject <info@blogsecurity.com>
2024-10-17 14:10:05 +0300

    Initial commit: Add basic plugin structure

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name: My First Plugin
+ * Plugin URI: https://github.com/yourusername/my-first-plugin
+ * Description: A simple WordPress plugin developed using GitHub.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: https://github.com/yourusername
+ * License: GPL2
+ * */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
